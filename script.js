e749c88b3edefc8b23c9c6713f20a9db639e7a6e
Aditya chandel <adityachandel371@gmail.com>
2025-01-02 19:55:55 +0530

    Added js logic for theme

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let output = "";
 const calculate = (btnValue) => {
     if (btnValue === "=" && output !== "") {
         output = new Function("return " + output.replace(/%/g, "/100"))();
-    } else if (btnValue === "AC") {
+    } else if (btnValue === "C") {
         output = "";
     } else if (btnValue === "DEL") {
         output = output.toString().slice(0, -1);
@@ -20,3 +20,15 @@ const calculate = (btnValue) => {
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+
+themeToggleBtn.onclick = () => {
+    calculator.classList.toggle("dark");
+    themeToggleBtn.classList.toggle("active");
+    toggleIcon.classList.toggle("active");
+    isDark = !isDark;
+};
